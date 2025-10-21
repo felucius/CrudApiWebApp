6924afe5b43db6f505d86e29ad0e0acc7be3d095
felucius <maxime.de.lange@outlook.com>
2025-10-21 11:53:49 +0200

    Added extra router option for approuter

--- web-app/src/server/api/root.ts
+++ web-app/src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { exampleRouter } from "./routers/example";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   post: postRouter,
+  example: exampleRouter
 });
 
 // export type definition of API
